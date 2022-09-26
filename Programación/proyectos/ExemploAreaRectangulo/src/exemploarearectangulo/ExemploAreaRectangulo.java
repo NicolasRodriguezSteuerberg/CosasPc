2faaea785310495947d26f420e7e66d26145e67d
NicolasRodriguezSteuerberg <nrodriguezsteuerberg@danielcastelao.org>
2022-09-26 13:10:09 +0200

    paso1

---
+++ Programación/proyectos/ExemploAreaRectangulo/src/exemploarearectangulo/ExemploAreaRectangulo.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package exemploarearectangulo;
+
+/**
+ *
+ * @author dam1
+ */
+public class ExemploAreaRectangulo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // proxecto que calcula o area dun rectangulo de base = 2 = b e altura = 5 = h
+        float base=2f , altura= 5.0f, area; //son variables locais
+        area = base*altura;
+        System.out.println("area ="+ area);
+        
+    }
+    
+}
