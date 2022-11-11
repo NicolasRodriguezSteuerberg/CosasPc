716c3cfc351f590288f65d313df710d6a481b574
NicolasRodriguezSteuerberg <nrodriguezsteuerberg@danielcastelao.org>
2022-11-11 09:34:36 +0100

    11nov

--- Contornos de datos/aaaaaaaaaaaaaa/src/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        int a=0;
-        System.out.println("Hello world!");
-        for (int i=0; i<123; i++){
-            a=a*2+i;
-        }
-    }
-}
