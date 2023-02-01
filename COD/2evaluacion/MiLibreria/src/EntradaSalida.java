d76d7da9366492ecafb1c439d4fb2c2211bf76d2
NicolasRodriguezSteuerberg <nrodriguezsteuerberg@danielcastelao.org>
2023-02-01 14:33:40 +0100

    1feb

---
+++ COD/2evaluacion/MiLibreria/src/EntradaSalida.java
@@ -0,0 +1,42 @@
+import javax.swing.*;
+
+public class EntradaSalida {
+
+    static final int SALIDA_CONSOLA = 1;
+    static final int SALIDA_WINDOW = 2;
+
+    /**
+     * Salida por ventana o consala de un mensaje
+     * @param msj cadena que queremos imprimir
+     * @param device dispositivo de salida, consola (SALIDA_CONSOLA) o ventana (SALIDA_VENTANA)
+     * @return si es true: correcto si no false
+     */
+    static boolean salida(String msj, int device) {
+        switch (device) {
+            case SALIDA_CONSOLA:
+                try {
+                    System.out.println("Consola: " + msj);
+                    return true;
+                } catch (Exception e) {
+                    return false;
+                }
+
+            case SALIDA_WINDOW:
+                try {
+                    JOptionPane.showMessageDialog(null,"Sale por un ventana " + msj.toCharArray()[10]);
+                    return true;
+                } catch(Exception e) {
+                    System.out.println(e.getMessage());
+                    return false;
+                }
+
+            default:
+                return false;
+        }
+    }
+
+    //TODO entrada
+    /*static String entrada(String comentario){
+        return "";
+    }*/
+}
