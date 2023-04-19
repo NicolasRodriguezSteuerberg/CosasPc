f86736c129266721c1ca206bdd4624f17a3586cd
NicolasRodriguezSteuerberg <nrodriguezsteuerberg@danielcastelao.org>
2023-04-19 13:28:12 +0200

    Calculadora+BonoLoto

---
+++ Programación/3evaluacion/boletines/Boletin31/Boletin31/src/boletin31/Boletin31.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package boletin31;
+
+/**
+ *
+ * @author dam1
+ */
+public class Boletin31 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        VentanaCalculadora obx = new VentanaCalculadora();
+        obx.setVisible(true);
+    }
+    
+}
---
+++ Programación/3evaluacion/boletinesExtras/BonoLoto/src/bonoloto/BonoLoto.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package bonoloto;
+
+/**
+ *
+ * @author dam1
+ */
+public class BonoLoto {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Ventana obx = new Ventana();
+        obx.setVisible(true);
+    }
+    
+}
