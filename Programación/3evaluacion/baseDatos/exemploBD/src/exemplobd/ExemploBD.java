729162f7cff98fcf45a55713cf9833776e15e7e5
Nicolas Rodriguez Steuerberg <nrodriguezsteuerberg@danielcastelao.org>
2023-05-09 16:36:13 +0200

    a

--- Programación/3evaluacion/baseDatos/exemploBD/src/exemplobd/ExemploBD.java
+++ Programación/3evaluacion/baseDatos/exemploBD/src/exemplobd/ExemploBD.java
@@ -16,6 +16,7 @@ public class ExemploBD {
     public static void main(String[] args) {
         // TODO code application logic here
         InterfazGrafica obxUI = new InterfazGrafica();
+        
     }
     
 }
