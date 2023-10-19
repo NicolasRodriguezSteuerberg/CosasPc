42be68bae790001af610cf0797c3f7abee7f14ab
Nico <nrodriguezsteuerberg@danielcastelao.org>
2023-10-19 11:53:53 +0200

    modulando la interfaz de usuario

--- 2/PMDM/Ejemplos/1Ejemplo/app/src/main/java/com/nsteuerberg/primerintento/ui/theme/UI.kt
+++ 2/PMDM/Ejemplos/1Ejemplo/app/src/main/java/com/nsteuerberg/primerintento/ui/theme/UI.kt
@@ -15,8 +15,6 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
@@ -27,10 +25,6 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.nsteuerberg.primerintento.R
 
-@OptIn(ExperimentalMaterial3Api::class)
-open class UI {
-}
-
 @Composable
  fun InterfazUsuario(miViewModel: MyViewModel, name: String, modifier: Modifier = Modifier) {
     //var numbers = remember { mutableStateOf(0) }  // esto hace que siempre que cambie el valor de numbers se va a actualizar, remember es como un repintar
@@ -39,78 +33,94 @@ open class UI {
 
     var greetings = stringResource(R.string.greetings)
 
-
     Column {
-        Row {
-            Text(
-                text = "$greetings, $name!",
-                modifier = modifier,
-                fontSize = 25.sp,
-                lineHeight = 90.sp,
-                color = Color.Red,
-                textAlign = TextAlign.Center
-            )
-        }
-        Row(modifier = Modifier.padding(0.dp,10.dp)) {
+        // están separado por filas
+        fInicio(greetings = greetings, name = name, modifier = modifier)
+
+        fButton(miViewModel = miViewModel)
+
+        fTextoRellenar(miViewModel = miViewModel)
+    }
+}
+
+@Composable
+fun fInicio(greetings: String, name: String, modifier: Modifier){
+    Row {
+        Text(
+            text = "$greetings, $name!",
+            modifier = modifier,
+            fontSize = 25.sp,
+            lineHeight = 90.sp,
+            color = Color.Red,
+            textAlign = TextAlign.Center
+        )
+    }
+    Row(modifier = Modifier.padding(0.dp,10.dp)) {
+        Image(
+            painter = painterResource(id = R.drawable.dino),
+            contentDescription = "icono de android",
+            modifier = Modifier
+                .padding(30.dp, 0.dp)
+        )
+    }
+}
+@Composable
+fun fButton(miViewModel: MyViewModel){
+    Row {
+        Text(
+            text = "Lista: ${miViewModel.getLista()}"
+        )
+    }
+    Row (modifier = Modifier.padding(0.dp,10.dp)) {
+        Button(
+            onClick = {
+                //miViewModel.crearRandom()
+                miViewModel.crearListaRandom()
+                Log.d("Funciones", "Click!!!!!!!!!")
+            },
+            modifier = Modifier
+                .height(100.dp)
+                .width(230.dp)
+                .padding(30.dp, 0.dp),
+            colors = ButtonDefaults.buttonColors(Color.Yellow)
+        ) { // es un metodo que puede recibir el boton, al ser el último lo podemos poner fuera de los paréntesis
             Image(
                 painter = painterResource(id = R.drawable.dino),
-                contentDescription = "icono de android",
-                modifier = Modifier
-                    .padding(30.dp,0.dp)
+                contentDescription = "Icono boton"
             )
-        }
-        Row {
             Text(
-                text = "Lista: ${miViewModel.getLista()}"
+                text = "Click me!", textAlign = TextAlign.Center,
+                color = Color.Cyan
             )
-        }
-        Row (modifier = Modifier.padding(0.dp,10.dp)){
-            Button(
-                onClick = {
-                    //miViewModel.crearRandom()
-                    miViewModel.crearListaRandom()
-                    Log.d("Funciones", "Click!!!!!!!!!")
-                },
-                modifier = Modifier
-                    .height(100.dp)
-                    .width(230.dp)
-                    .padding(30.dp, 0.dp),
-                colors = ButtonDefaults.buttonColors(Color.Yellow)
-            ){
-                Image(
-                    painter = painterResource(id = R.drawable.dino),
-                    contentDescription = "Icono boton"
-                )
-                Text(
-                    text = "Click me!", textAlign = TextAlign.Center,
-                    color = Color.Cyan
-                )
-            }
-        }
-        Row{
-            // campo de texto para rellenar
-            OutlinedTextField(
-                value = miViewModel.getNombre(),
-                onValueChange = {
-                    // preguntar a gabriel por que se hace esto, ¿Esto no es privado?
-                    miViewModel._nameC.value = it
-                },
-                label = { Text(
-                    text = "Introduzca un nombre",
-                    color = Color.Red
-                )
-                }
-            )
-        }
-        Row(modifier = Modifier.padding(15.dp,0.dp)){
-            if(miViewModel.getNombre().length>3){
-                Text(
-                    text = "Hola: ${miViewModel.getNombre()}!"
-                )
-            }
         }
     }
 }
+@Composable
+fun fTextoRellenar(miViewModel: MyViewModel){
+    Row{
+        // campo de texto para rellenar
+        OutlinedTextField(
+            value = miViewModel.getNombre(),
+            onValueChange = {
+                // preguntar a gabriel por que se hace esto, ¿Esto no es privado?
+                miViewModel._nameC.value = it //variable string
+            },
+            label = { Text(
+                text = "Introduzca un nombre",
+                color = Color.Red
+            )
+            }
+        )
+    }
+    Row{
+        if(miViewModel.getNombre().length>3){
+            Text(
+                text = "Hola: ${miViewModel.getNombre()}!"
+            )
+        }
+    }
+}
+
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
