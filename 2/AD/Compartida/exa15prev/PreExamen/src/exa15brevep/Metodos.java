793f61d02510bc7abeaa78df1bddefdaf23e2207
Nico <nrodriguezsteuerberg@danielcastelao.org>
2023-10-25 14:33:38 +0200

    25oct

--- 2/AD/Compartida/exa15prev/PreExamen/src/exa15brevep/Metodos.java
+++ 2/AD/Compartida/exa15prev/PreExamen/src/exa15brevep/Metodos.java
@@ -2,6 +2,7 @@ package exa15brevep;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.sql.Connection;
@@ -9,6 +10,9 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import javax.xml.stream.XMLOutputFactory;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.XMLStreamWriter;
 
 
 public class Metodos {
@@ -27,10 +31,19 @@ public class Metodos {
         return conn;
     }
     
-    public void lecturaSerializada(String ruta) throws FileNotFoundException, IOException, ClassNotFoundException, SQLException{
+    public void lecturaSerializada(String ruta, String rutaEsc) throws FileNotFoundException, IOException, ClassNotFoundException, SQLException, XMLStreamException{
         Connection con = conexion();
         ObjectInputStream lec = null;
         Platos obx = null;
+        
+        // para escribir el xml
+        FileWriter obxFile = new FileWriter(rutaEsc);
+        XMLOutputFactory obxFactory = XMLOutputFactory.newInstance();
+        XMLStreamWriter obxWriter = obxFactory.createXMLStreamWriter(obxFile);
+        
+        // empezamos el xml
+        obxWriter.writeStartDocument("1.0");
+        obxWriter.writeStartElement("platos");
 
         lec = new ObjectInputStream(new FileInputStream(ruta));
         
@@ -38,17 +51,24 @@ public class Metodos {
             obx = (Platos) lec.readObject();
             if(obx != null){
                 System.out.println(obx.getCodigop() + "\n" + obx.getNomep());
-                lecturaDatos(con,obx.getCodigop());
+                obxWriter.writeStartElement("plato");
+                    obxWriter.writeAttribute("codigop", obx.getCodigop());
+                    obxWriter.writeStartElement("nomep");
+                        obxWriter.writeCharacters(obx.getNomep());
+                    obxWriter.writeEndElement();
+                lecturaDatos(con,obx.getCodigop(), obxWriter);
+                obxWriter.writeEndElement();
             }
             else{
                 System.out.println("Objeto nulo");
             }
         }while(obx != null);
-            
+        obxWriter.writeEndElement();
+        obxWriter.close();
         lec.close();
     }
     
-    public void lecturaDatos(Connection con,String codp) throws SQLException{
+    public void lecturaDatos(Connection con,String codp, XMLStreamWriter obxWriter) throws SQLException, XMLStreamException{
         int peso;
         int graxa;
         String codc;
@@ -76,6 +96,9 @@ public class Metodos {
             
         }
         System.out.println("graxatotal: " + graxa_total);
+        obxWriter.writeStartElement("graxatotal");
+            obxWriter.writeCharacters(String.valueOf(graxa_total));
+        obxWriter.writeEndElement();
         
     }
 }
--- 2/AD/Compartida/exa15prev/PreExamen/src/exa15brevep/PreExamen.java
+++ 2/AD/Compartida/exa15prev/PreExamen/src/exa15brevep/PreExamen.java
@@ -3,15 +3,17 @@ package exa15brevep;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.sql.SQLException;
+import javax.xml.stream.XMLStreamException;
 
 
 public class PreExamen {
 
-    public static void main(String[] args) throws IOException, ClassNotFoundException, FileNotFoundException, SQLException, SQLException, SQLException, SQLException {
+    public static void main(String[] args) throws IOException, ClassNotFoundException, FileNotFoundException, SQLException, SQLException, SQLException, SQLException, XMLStreamException {
         // TODO code application logic here
         Metodos obx = new Metodos();
         String ruta = "/media/sf_Compartida/exa15prev/platoss";
-        obx.lecturaSerializada(ruta);
+        String rutaEsc = "/media/sf_Compartida/exa15prev/platos.xml";
+        obx.lecturaSerializada(ruta, rutaEsc);
     }
     
 }
