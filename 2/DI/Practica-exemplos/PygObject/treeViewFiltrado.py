3c327a03f0e205cc99a53c99bd1b612e44b4d701
Nico <nrodriguezsteuerberg@danielcastelao.org>
2023-12-11 13:42:17 +0100

    11dic

--- 2/DI/Practica-exemplos/PygObject/treeViewFiltrado.py
+++ 2/DI/Practica-exemplos/PygObject/treeViewFiltrado.py
@@ -16,8 +16,12 @@ class FestraPrincipal(Gtk.Window):
 
         caixa = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=4)
 
+        self.filtradoXenero="None"
+
         # Establecemos el tipo que tendra en cada columna
         modelo = Gtk.ListStore(str, str, int, str, bool)
+        modelo_filtrado = modelo.filter_new()
+        modelo_filtrado.set_visible_func(self.filtrar_usuarios_xenero)
         try:
             database = dbapi.connect("baseDatos2.dat")
             cursor = database.cursor()
@@ -32,7 +36,8 @@ class FestraPrincipal(Gtk.Window):
             cursor.close()
             database.close()
 
-        self.miTreeView = Gtk.TreeView(model=modelo)
+        #self.miTreeView = Gtk.TreeView(model=modelo)
+        self.miTreeView = Gtk.TreeView(model=modelo_filtrado)
         selection = self.miTreeView.get_selection()
         for i, nombreColumna in enumerate(["DNI", "Nombre", "Edad"]):
             celda = Gtk.CellRendererText()
@@ -55,11 +60,27 @@ class FestraPrincipal(Gtk.Window):
         # celdaCombo.set_property("model", modeloCombo)
         celdaCombo.set_property("text-column", 0) # esto es el indice de la columna
         celdaCombo.set_property("has-entry", False)
+        celdaCombo.connect("edited", self.on_celdaCombo_edited, modelo_filtrado, 3)
         columna = Gtk.TreeViewColumn("Xenero", celdaCombo, text=3)
         self.miTreeView.append_column(columna)
 
-
         caixa.pack_start(self.miTreeView, True, True, 0)
+
+        caixaH = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=2)
+        caixa.pack_start(caixaH, True, True, 0)
+        rbtAll = Gtk.RadioButton.new_with_label(None, "All")
+        rbtHome = Gtk.RadioButton.new_with_label_from_widget(rbtAll, "Home")
+        rbtMuller = Gtk.RadioButton.new_with_label_from_widget(rbtAll, "Muller")
+        rbtOutros = Gtk.RadioButton.new_with_label_from_widget(rbtAll, "Outros")
+        caixaH.pack_start(rbtAll, True, True, 2)
+        caixaH.pack_start(rbtHome, True, True, 2)
+        caixaH.pack_start(rbtMuller, True, True, 2)
+        caixaH.pack_start(rbtOutros, True, True, 2)
+
+        rbtAll.connect("toggled", self.on_rbt_toggled, "None", modelo_filtrado)
+        rbtHome.connect("toggled", self.on_rbt_toggled, "Home", modelo_filtrado)
+        rbtMuller.connect("toggled", self.on_rbt_toggled, "Muller", modelo_filtrado)
+        rbtOutros.connect("toggled", self.on_rbt_toggled, "Outros", modelo_filtrado)
 
         self.add(caixa)
 
@@ -67,6 +88,40 @@ class FestraPrincipal(Gtk.Window):
 
         self.show_all()
 
+    def on_celdaCombo_edited(self, celda, fila, texto, modelo, indice):
+        cambiado = self.modificarComboBD(texto,modelo,fila)
+        if cambiado:
+            modelo[fila][indice] = texto
+        else:
+            print("Error al cambiar")
+
+    def modificarComboBD(self, texto, modelo, fila):
+        try:
+            database = dbapi.connect("baseDatos2.dat")
+            cursor = database.cursor()
+            cursor.execute("update usuarios set xenero = ? where dni = ?", (texto, modelo[fila][0]))
+            database.commit()
+            return True
+        except dbapi.StandardError as e:
+            print(e)
+            return False
+        except dbapi.DatabaseError as e:
+            print(e)
+            return False
+        finally:
+            cursor.close()
+            database.close()
+
+    def on_rbt_toggled(self, botonSeleccionado, genero, modelo):
+        if botonSeleccionado.get_active():
+            self.filtradoXenero = genero
+            modelo.refilter()
+
+    def filtrar_usuarios_xenero(self, modelo, fila, datos):
+        if self.filtradoXenero == "None" or self.filtradoXenero is None:
+            return True
+        else:
+            return modelo[fila][3] == self.filtradoXenero
 
 if __name__ == "__main__":
     FestraPrincipal()
--- 2/SXE/Wordpress2/html/wp-content/plugins/words/words.php
+++ 2/SXE/Wordpress2/html/wp-content/plugins/words/words.php
@@ -12,6 +12,7 @@ Version: 0.0.1
 Author URI: http://ma.tt/
 */
 
+
 # list of offensive words
 $offensiveWordsList = [
     "mierda",
@@ -42,13 +43,33 @@ $nonOffensiveWordsList = [
  * it will be replaced by WordPressDAM.
  * @param $text string
  * @return string
- */
+
 function renym_wordpress_typo_fix( $text ) {
     global $offensiveWordsList, $nonOffensiveWordsList;
     return str_replace( $offensiveWordsList, $nonOffensiveWordsList, $text );
 }
+*/
 
-add_filter( 'the_content', 'renym_wordpress_typo_fix' );
+/**
+ * Whenever the word WordPress appears in the content
+ * of a post or a page,
+ * it will be replaced by WordPressDAM.
+ * This time using the database
+ * @param $text string
+ * @return string
+ */
+function renym_wordpress_typo_fix($text){
+    // take the words from the table
+    $words = selectData();
+    foreach ($words as $result){
+        $offensiveWordsList[] = $result->offensiveWords; // -> para seleccionar que columna escoger
+        $nonOffensiveWordsList[] = $result->nonOffensiveWords;
+    }
+    return str_replace($offensiveWordsList, $nonOffensiveWordsList, $text);
+}
+
+
+add_filter('the_content', 'renym_wordpress_typo_fix');
 /*
 function nico_words_get_lyric() {
     /** These are the lyrics to Nico Words
@@ -58,3 +79,54 @@ function nico_words_get_lyric() {
     return wptexturize( $lyrics );
 }
 */
+
+/**
+ * To do this but with databases,
+ * First, we will create the table
+ */
+function createTable(){
+    global $wpdb; // this is how you get access to the database
+    $table_name = $wpdb->prefix . "nicoWords";
+    //Charset de la tabla
+    $charset_collate = $wpdb->get_charset_collate();
+    //Sentencia SQL
+    $sql = "CREATE TABLE IF NOT EXISTS $table_name (
+        id mediumint(9) NOT NULL AUTO_INCREMENT,
+        offensiveWords varchar(255) NOT NULL,
+        nonOffensiveWords varchar(255) NOT NULL,
+        PRIMARY KEY  (id)
+    ) $charset_collate;";
+    //Incluir el fichero para poder ejecutar dbDelta
+    require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+    dbDelta( $sql );
+}
+// when the plugin is activated, we create the table
+add_action("plugins_loaded", "createTable");
+
+// now we insert the words into the table
+function insertData(){
+    global $wpdb, $offensiveWordsList, $nonOffensiveWordsList;
+    $table_name = $wpdb->prefix . "nicoWords";
+    $flag = $wpdb->get_results("SELECT * FROM $table_name");
+    if (count($flag)==0){
+        for ($i = 0; $i < count($offensiveWordsList); $i++){
+            $wpdb->insert(
+                $table_name,
+                array(
+                    'offensiveWords' => $offensiveWordsList[$i],
+                    'nonOffensiveWords' => $nonOffensiveWordsList[$i]
+                )
+            );
+        }
+    }
+}
+// when the plugin is activated, we insert the words
+add_action("plugins_loaded", "insertData");
+
+// selecting the words from the database
+function selectData(){
+    global $wpdb;
+    $table_name = $wpdb->prefix . 'nicoWords';
+    $results = $wpdb->get_results( "SELECT * FROM $table_name" );
+    return $results;
+}
